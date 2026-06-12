2f59cd2029aa058c9acb2fcf33482ea3b67cf1ca
nehajaiswal161 <nehajaiswal0602@gmail.com>
2026-06-12 14:08:01 +0530

    Create cartSlice.js

---
+++ src/store/cartSlice.js
@@ -0,0 +1,65 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: JSON.parse(localStorage.getItem("cart")) || [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = state.items.find(
+        (i) => i.id === action.payload.id
+      );
+
+      if (item) {
+        item.quantity += 1;
+      } else {
+        state.items.push({
+          ...action.payload,
+          quantity: 1,
+        });
+      }
+
+      localStorage.setItem(
+        "cart",
+        JSON.stringify(state.items)
+      );
+    },
+
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.id !== action.payload
+      );
+
+      localStorage.setItem(
+        "cart",
+        JSON.stringify(state.items)
+      );
+    },
+
+    updateQuantity: (state, action) => {
+      const item = state.items.find(
+        (i) => i.id === action.payload.id
+      );
+
+      if (item) {
+        item.quantity = action.payload.quantity;
+      }
+
+      localStorage.setItem(
+        "cart",
+        JSON.stringify(state.items)
+      );
+    },
+  },
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  updateQuantity,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
